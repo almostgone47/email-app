27b91c3849314b8a28a31e2bab3a8605aa426ac7
almostgone47 <almostgone47@hotmail.com>
2020-05-05 17:50:45 +1000

    completed auth

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '909205543857-l1oun21bnlc3nmgrg095jr0mvsvdn2k6.apps.googleusercontent.com',
+    googleClientSecret: '5xOz6bnj_wg84lhZD6tzCPyg',
+    cookieKey: 'lakdjfakdhaldjshfaslkfjhsdaflkjasfhg',
+    mongodbKey: 'mongodb+srv://jeremiah:jeremiah@cluster0-9vxcq.mongodb.net/test?retryWrites=true&w=majority'
+};
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const keys = require('./config/keys');
+require('./models/Users');
+require('./services/passport');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+mongoose.connect('mongodb://localhost/email-app', { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [ keys.cookieKey ]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT)
+app.listen(PORT);
